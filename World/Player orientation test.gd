2e10468dacfa8e706acd428ab62195debeb09e08
Bonono63 <bonoria63@gmail.com>
2023-08-30 19:24:08 -0400

    njiwenijefjnsdfjkjndf

--- World/Player orientation test.gd
+++ World/Player orientation test.gd
@@ -1,12 +1,26 @@
 extends RigidBody3D
 
-@export var locked_entity : Node3D
+var _move_direction := Vector3.ZERO
+var last_strong_direction := Vector3.FORWARD
+var local_gravity = Vector3.DOWN
 
-func _process(_delta):
-	transform.basis = Basis()
-	var target_position = locked_entity.position
-	var looking_transform = transform.looking_at(target_position, Vector3(0,1,0), true)
-	print(looking_transform.basis)
-	looking_transform.
-	transform = looking_transform
-	transform.orthonormalized()
+func _integrate_forces(state):
+	if _move_direction.length() > 0.2:
+		last_strong_direction = _move_direction.normalized()
+	
+	_move_direction = _get_model_oriented_input()
+	
+	local_gravity = state.total_gravity.normalized()
+	_orient_character_to_direction(last_strong_direction, state.step)
+
+func _orient_character_to_direction(direction : Vector3, delta : float) -> void:
+	var left_axis : Vector3 = -local_gravity.cross(direction)
+	var rotation_quat := Quaternion(Basis(left_axis, -local_gravity, direction).orthonormalized())
+	var _transform := Quaternion(transform.basis.orthonormalized())
+	
+	var c = _transform.slerp(rotation_quat, delta * 8 )
+	
+	transform.basis = Basis(c)
+
+func _get_model_oriented_input():
+	return Vector3.ZERO
